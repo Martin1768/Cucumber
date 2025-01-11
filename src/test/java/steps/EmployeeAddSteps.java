d111cddb3344c8910e7fd4e27f8baaa57ddf5fc6
Martin1768 <marsch1768.sdet@gmail.com>
2025-01-11 20:42:44 +0100

    Add Employee - data-driven without Excel feature+steps

--- src/test/java/steps/EmployeeAddSteps.java
+++ src/test/java/steps/EmployeeAddSteps.java
@@ -6,12 +6,16 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import utils.CommonMethods;
 
+import java.util.List;
+import java.util.Map;
+
 public class EmployeeAddSteps extends CommonMethods {
     @When("user clicks on Add employee option")
     public void user_clicks_on_add_employee_option() {
         WebElement addEmpOption = driver.findElement(By.id("menu_pim_addEmployee"));
         addEmpOption.click();
     }
+
     @When("user enters firstname and lastname")
     public void user_enters_firstname_and_lastname() {
         WebElement firstNameField = driver.findElement(By.id("firstName"));
@@ -19,6 +23,7 @@ public class EmployeeAddSteps extends CommonMethods {
         firstNameField.sendKeys("mark");
         lastNameField.sendKeys("jacob");
     }
+
     @When("user enters firstname and middlename and lastname")
     public void user_enters_firstname_and_middlename_and_lastname() {
         WebElement firstNameField = driver.findElement(By.id("firstName"));
@@ -28,11 +33,54 @@ public class EmployeeAddSteps extends CommonMethods {
         midNameField.sendKeys("ms");
         lastNameField.sendKeys("jacob");
     }
+
+    @When("user enters {string} and {string} and {string} in the application")
+    public void user_enters_and_and_in_the_application(String fn, String mn, String ln) {
+        WebElement firstNameField = driver.findElement(By.id("firstName"));
+        WebElement midNameField = driver.findElement(By.id("middleName"));
+        WebElement lastNameField = driver.findElement(By.id("lastName"));
+        firstNameField.sendKeys(fn);
+        midNameField.sendKeys(mn);
+        lastNameField.sendKeys(ln);
+    }
+
+    @When("user adds {string} , {string} and {string}")
+    public void user_adds_and(String fn, String mn, String ln) {
+        WebElement firstNameField = driver.findElement(By.id("firstName"));
+        WebElement midNameField = driver.findElement(By.id("middleName"));
+        WebElement lastNameField = driver.findElement(By.id("lastName"));
+        firstNameField.sendKeys(fn);
+        midNameField.sendKeys(mn);
+        lastNameField.sendKeys(ln);
+    }
+
+    @When("user adds multiple employees using table and save them")
+    public void user_adds_multiple_employees_using_table_and_save_them(
+            io.cucumber.datatable.DataTable dataTable) {
+        List<Map<String, String>> employeeNames = dataTable.asMaps();
+        for (Map<String, String> employee:employeeNames) {
+            WebElement firstNameField = driver.findElement(By.id("firstName"));
+            WebElement midNameField = driver.findElement(By.id("middleName"));
+            WebElement lastNameField = driver.findElement(By.id("lastName"));
+            firstNameField.sendKeys(employee.get("firstName"));
+            midNameField.sendKeys(employee.get("middleName"));
+            lastNameField.sendKeys(employee.get("lastName"));
+
+            WebElement saveBtn = driver.findElement(By.id("btnSave"));
+            saveBtn.click();
+
+            WebElement addEmpOption = driver.findElement(By.id("menu_pim_addEmployee"));
+            addEmpOption.click();
+
+        }
+    }
+
     @When("user clicks on save button")
     public void user_clicks_on_save_button() {
         WebElement saveBtn = driver.findElement(By.id("btnSave"));
         saveBtn.click();
     }
+
     @Then("employee added successfully")
     public void employee_added_successfully() {
         System.out.println("employee added successfully");
--- src/test/resources/features/AddEmployee.feature
+++ src/test/resources/features/AddEmployee.feature
@@ -1,15 +1,15 @@
 Feature: Add epmployee
 
-Background:
+  Background:
   #Given user is able to access HRMS application   // hooks opens the app
-  When user enters admin username and password
-  And user clicks on login button
-  Then user is able to see dashboard page
-  When user clicks on PIM button
-  And user clicks on Add employee option
+    When user enters admin username and password
+    And user clicks on login button
+    Then user is able to see dashboard page
+    When user clicks on PIM button
+    And user clicks on Add employee option
 
   @addemployee @add2names
-    Scenario: Adding the employee by firstname and lastname
+  Scenario: Adding the employee by firstname and lastname
     And user enters firstname and lastname
     And user clicks on save button
     Then employee added successfully
@@ -19,4 +19,33 @@ Background:
     And user enters firstname and middlename and lastname
     And user clicks on save button
     Then employee added successfully
+
+  @addemployeebyparams
+  Scenario: Adding employee using parameters
+    And user enters "mark" and "anthony" and "jacob" in the application
+    And user clicks on save button
+    Then employee added successfully
+
+  @addemployeebyhardtable
+  Scenario Outline: Adding multiple employees
+    And user adds "<firstname>" , "<middlename>" and "<lastname>"
+    And user clicks on save button
+    Then employee added successfully
+    Examples:
+      | firstname | middlename | lastname |
+      | mark      | ms         | jacob    |
+      | adam      | ms         | jacob    |
+      | steve     | ms         | jacob    |
+
+  @addemployeebydata
+  Scenario: Adding multiple employees using data table
+    When user adds multiple employees using table and save them
+      | firstName | middleName | lastName |
+      | mark      | ms         | jacob    |
+      | adam      | ms         | jacob    |
+      | steve     | ms         | jacob    |
+
+
+
+
 
