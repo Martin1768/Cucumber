48f9c64faf07a2ba98983c1cf61ece21540cfdb4
Martin1768 <marsch1768.sdet@gmail.com>
2025-01-11 16:48:34 +0100

    Constants update

--- src/test/java/utils/Constants.java
+++ src/test/java/utils/Constants.java
@@ -1,6 +1,8 @@
 package utils;
 
 public class Constants {
-    public static final String EXCEL_FILE_PATH =System.getProperty("user.dir")+"Files/data.xlsx";
+    public static final String EXCEL_FILE_PATH =System.getProperty("user.dir")+"Files/excel/";
+    public static final String CSV_FILE_PATH =System.getProperty("user.dir")+"Files/csv/";
+    public static final String SCREENSHOTS_PATH =System.getProperty("user.dir")+"Files/screenshots/";
     public static final String CONFIG_FILE_PATH=System.getProperty("user.dir")+"/src/test/resources/config/config.properties";
 }
