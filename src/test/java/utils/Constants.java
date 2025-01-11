7db67ff6ddff40989cd3f7054955d8ad9ae3e80c
Martin1768 <marsch1768.sdet@gmail.com>
2025-01-11 23:50:20 +0100

    Change structure to Framework rules

--- src/test/java/utils/Constants.java
+++ src/test/java/utils/Constants.java
@@ -1,8 +1,8 @@
 package utils;
 
 public class Constants {
-    public static final String EXCEL_FILE_PATH =System.getProperty("user.dir")+"/Files/excel/";
-    public static final String CSV_FILE_PATH =System.getProperty("user.dir")+"/Files/csv/";
-    public static final String SCREENSHOTS_PATH =System.getProperty("user.dir")+"/Files/screenshots/";
+    public static final String EXCEL_FILE_PATH =System.getProperty("user.dir")+"/src/test/resources/excel/";
+    public static final String CSV_FILE_PATH =System.getProperty("user.dir")+"/src/test/resources/csv/";
+    public static final String SCREENSHOTS_PATH =System.getProperty("user.dir")+"/src/test/resources/screenshots/";
     public static final String CONFIG_FILE_PATH=System.getProperty("user.dir")+"/src/test/resources/config/config.properties";
 }
